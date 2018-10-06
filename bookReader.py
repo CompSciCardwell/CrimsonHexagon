83d6cf8c37c81e81b3dcd3bb7778b24b28f6f33e
CompSciCardwell <bcardwell1@student.gsu.edu>
2018-10-06 18:41:50 -0400

    creating dictionary

--- bookReader.py
+++ bookReader.py
@@ -2,33 +2,33 @@ import ebooklib
 from ebooklib import epub
 from pathlib import Path
 from bs4 import BeautifulSoup
-
-def dumpDirectoryOfBooks( location ):
-    rootdir = Path(location)
-
-    file_list = [f for f in rootdir.glob('**/*') if f.is_file() and f.suffix == '.epub']
-
-    for f in file_list:
-        print(f)
-        book = epub.read_epub(f)
+import re
 
 def singleBook( location ):
     rootdir = Path(location)
-
-    file_list = [f for f in rootdir.glob('**/*') if f.is_file() and f.suffix == '.epub']
-
-    for f in file_list:
-        print(f)
-        book = epub.read_epub(f)
-
-rootdir = Path('/home/compscicardwell/SpiderOak Hive/Actual Documents/Git/CrimsonHexagon/Data Sets/Fantasy')
-
-file_list = [f for f in rootdir.glob('**/*') if f.is_file() and f.suffix == '.epub']
-
-for f in file_list:
-    print(f)
-    book = epub.read_epub(f)
+    book = epub.read_epub(rootdir)
     for item in book.get_items_of_type(ebooklib.ITEM_DOCUMENT):
-        print('NAME : ', item.get_name())
         cleantext = BeautifulSoup(item.get_content(), "lxml").text
+        cleantext = re.sub(r'[^\w\s]', '', cleantext)
+        cleantext = cleantext.lower()
         print(cleantext)
+
+def addToDict( dictionary, inputText ):
+    
+
+set = {}
+
+rootdir = Path('/home/compscicardwell/SpiderOak Hive/Actual Documents/Git/CrimsonHexagon/Data Sets/Fantasy/Eric - Terry Pratchett.epub')
+
+singleBook(rootdir)
+
+#file_list = [f for f in rootdir.glob('**/*') if f.is_file() and f.suffix == '.epub']
+
+#for f in file_list:
+    #print(f)
+    #book = epub.read_epub(f)
+    #for item in book.get_items_of_type(ebooklib.ITEM_DOCUMENT):
+        #print('NAME : ', item.get_name())
+        #cleantext = BeautifulSoup(item.get_content(), "lxml").text
+        #cleantext = re.sub(r'[^\w\s]', '', cleantext)
+        #print(cleantext.lower())
