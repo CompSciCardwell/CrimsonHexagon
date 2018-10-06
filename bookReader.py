29f12386746616419b5eb6ce3fb1e08ef4a47255
CompSciCardwell <bcardwell1@student.gsu.edu>
2018-10-06 16:51:57 -0400

    Getting closer to extracting the actual text

--- bookReader.py
+++ bookReader.py
@@ -1,6 +1,25 @@
 import ebooklib
 from ebooklib import epub
 from pathlib import Path
+from bs4 import BeautifulSoup
+
+def dumpDirectoryOfBooks( location ):
+    rootdir = Path(location)
+
+    file_list = [f for f in rootdir.glob('**/*') if f.is_file() and f.suffix == '.epub']
+
+    for f in file_list:
+        print(f)
+        book = epub.read_epub(f)
+
+def singleBook( location ):
+    rootdir = Path(location)
+
+    file_list = [f for f in rootdir.glob('**/*') if f.is_file() and f.suffix == '.epub']
+
+    for f in file_list:
+        print(f)
+        book = epub.read_epub(f)
 
 rootdir = Path('/home/compscicardwell/SpiderOak Hive/Actual Documents/Git/CrimsonHexagon/Data Sets/Fantasy')
 
@@ -9,3 +28,7 @@ file_list = [f for f in rootdir.glob('**/*') if f.is_file() and f.suffix == '.ep
 for f in file_list:
     print(f)
     book = epub.read_epub(f)
+    for item in book.get_items_of_type(ebooklib.ITEM_DOCUMENT):
+        print('NAME : ', item.get_name())
+        cleantext = BeautifulSoup(item.get_content(), "lxml").text
+        print(cleantext)
