606c24a83a5aeb1c1acf89497385bc5c14ea810e
CompSciCardwell <bcardwell1@student.gsu.edu>
2018-10-07 01:15:56 -0400

    almost got it to do words

--- bookReader.py
+++ bookReader.py
@@ -5,22 +5,41 @@ from bs4 import BeautifulSoup
 import re
 
 def singleBook( location ):
-    rootdir = Path(location)
-    book = epub.read_epub(rootdir)
-    for item in book.get_items_of_type(ebooklib.ITEM_DOCUMENT):
+    myDir = location
+    book = epub.read_epub(myDir)
+    items = book.get_items_of_type(ebooklib.ITEM_DOCUMENT)
+    outputtext = ""
+    for item in items:
         cleantext = BeautifulSoup(item.get_content(), "lxml").text
         cleantext = re.sub(r'[^\w\s]', '', cleantext)
         cleantext = cleantext.lower()
+        outputtext += cleantext
         print(cleantext)
+    return outputtext
+
 
 def addToDict( dictionary, inputText ):
-    
+    for line in inputText:
+        for word in line.split():
+            if not word in dictionary:
+                dictionary[word] = 1
+            else:
+                dictionary[word] += 1
+
+    for keys, values in dictionary.items():
+        print(keys)
+        print(values)
+
+    return dictionary
 
 set = {}
 
 rootdir = Path('/home/compscicardwell/SpiderOak Hive/Actual Documents/Git/CrimsonHexagon/Data Sets/Fantasy/Eric - Terry Pratchett.epub')
 
-singleBook(rootdir)
+#singleBook(rootdir)
+
+addToDict(set, singleBook(rootdir))
+print(set)
 
 #file_list = [f for f in rootdir.glob('**/*') if f.is_file() and f.suffix == '.epub']
 
